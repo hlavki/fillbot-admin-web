2d40b8f8f0daef7a36cf1ceff1e2e09db4e71c74
Michal Hlavac <michal.hlavac@mores.eu>
2023-09-14 21:51:55 +0200

    remove unused code

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -7,13 +7,11 @@ import { SharedModule } from '@shared/shared.module';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { BillingPageComponent } from './pages/billing-page/billing-page.component';
 import { PersonalPageComponent } from './pages/personal-page/personal-page.component';
-import { PaymentMethodDialogComponent } from './dialogs/payment-method-dialog/payment-method-dialog.component';
 
 @NgModule({
   declarations: [
     BillingPageComponent,
     PersonalPageComponent,
-    PaymentMethodDialogComponent,
   ],
   imports: [
     CommonModule,
