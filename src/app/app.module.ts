5f6212ec1d41dfde67bdd2a0c1329f20140f4a85
Michal Hlavac <michal.hlavac@mores.eu>
2025-01-16 20:29:23 +0100

    upgrade to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,73 +1,69 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TranslateCompiler, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { MatDialogConfig, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateMessageFormatCompiler } from 'ngx-translate-messageformat-compiler';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TranslateCompiler, TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
+import {MAT_DIALOG_DEFAULT_OPTIONS, MatDialogConfig} from '@angular/material/dialog';
+import {HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {TranslateMessageFormatCompiler} from 'ngx-translate-messageformat-compiler';
 
-import { httpLoaderFactory } from '@core/utils/httpLoaderFactory';
-import { initializeKeycloak } from '@core/utils/initKeycloak';
-import { SharedModule } from '@shared/shared.module';
+import {httpLoaderFactory} from '@core/utils/httpLoaderFactory';
+import {initializeKeycloak} from '@core/utils/initKeycloak';
+import {SharedModule} from '@shared/shared.module';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HttpErrorInterceptor } from '@core/interceptors/error.interceptor';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HttpErrorInterceptor} from '@core/interceptors/error.interceptor';
 
 import localeCs from '@angular/common/locales/cs';
 import localeSk from '@angular/common/locales/sk';
-import { registerLocaleData } from '@angular/common';
+import {registerLocaleData} from '@angular/common';
 
 registerLocaleData(localeCs, 'cs');
 registerLocaleData(localeSk, 'sk');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    KeycloakAngularModule,
-    AppRoutingModule,
-    SharedModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: httpLoaderFactory,
-        deps: [HttpClient],
-      },
-      compiler: {
-        provide: TranslateCompiler,
-        useClass: TranslateMessageFormatCompiler,
-      },
-      isolate: true,
-    }),
-  ],
-  providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true},
-    {
-      provide: APP_INITIALIZER,
-      multi: true,
-      deps: [KeycloakService],
-      useFactory: initializeKeycloak,
-    },
-    {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {...new MatDialogConfig(), width: '100%', maxWidth: '700px'}},
-  ],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        KeycloakAngularModule,
+        AppRoutingModule,
+        SharedModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: httpLoaderFactory,
+                deps: [HttpClient],
+            },
+            compiler: {
+                provide: TranslateCompiler,
+                useClass: TranslateMessageFormatCompiler,
+            },
+            isolate: true,
+        })], providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true},
+        {
+            provide: APP_INITIALIZER,
+            multi: true,
+            deps: [KeycloakService],
+            useFactory: initializeKeycloak,
+        },
+        {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: {...new MatDialogConfig(), width: '100%', maxWidth: '700px'}},
+        provideHttpClient(withInterceptorsFromDi()),
+    ],
 })
 export class AppModule {
-  private readonly ALLOWED_TRANSLATIONS: string[] = ['cs', 'sk'];
+    private readonly ALLOWED_TRANSLATIONS: string[] = ['cs', 'sk'];
 
-  constructor(
-    private readonly translateService: TranslateService,
-  ) {
-    if (this.ALLOWED_TRANSLATIONS.includes(navigator.language.substring(0, 2))) {
-      this.translateService.setDefaultLang(navigator.language.substring(0, 2));
-    } else {
-      this.translateService.setDefaultLang('en');
+    constructor(
+        private readonly translateService: TranslateService,
+    ) {
+        if (this.ALLOWED_TRANSLATIONS.includes(navigator.language.substring(0, 2))) {
+            this.translateService.setDefaultLang(navigator.language.substring(0, 2));
+        } else {
+            this.translateService.setDefaultLang('en');
+        }
     }
-  }
 }
